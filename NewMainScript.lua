83d7ff58acc7b781c518c4f5cff5865b43f2d78d
Night <joepa9000@gmail.com>
2025-08-12 21:43:42 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -3,30 +3,30 @@ if shared.vape then shared.vape:Uninject() end
 
 -- why do exploits fail to implement anything correctly? Is it really that hard?
 if identifyexecutor then
-	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
-		getgenv().setthreadidentity = nil
-	end
+		if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
+				getgenv().setthreadidentity = nil
+		end
 end
 
 local vape
 local loadstring = function(...)
-	local res, err = loadstring(...)
-	if err and vape then
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
-	end
-	return res
+		local res, err = loadstring(...)
+		if err and vape then
+				vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+		end
+		return res
 end
 
 local queue_on_teleport = queue_on_teleport or function() end
 local isfile = isfile or function(file)
-	local suc, res = pcall(function()
-		return readfile(file)
-	end)
-	return suc and res ~= nil and res ~= ''
+		local suc, res = pcall(function()
+				return readfile(file)
+		end)
+		return suc and res ~= nil and res ~= ''
 end
 
 local cloneref = cloneref or function(obj)
-	return obj
+		return obj
 end
 
 local playersService = cloneref(game:GetService('Players'))
@@ -34,111 +34,109 @@ local lplr = playersService.LocalPlayer :: Player
 local knit = lplr.PlayerScripts:FindFirstChild('TS') and lplr.PlayerScripts.TS:FindFirstChild('knit') :: ModuleScript?
 
 if knit and identifyexecutor() == 'Delta' and game.PlaceId ~= 6872265039 then
-    local Success, Main
-    
-    local FakeFunc = function() end
-    
-    repeat
-        Success, Main = pcall(function()
-          return debug.getupvalue(require(knit).setup, 9)
-        end)
-        task.wait()
-    until Success
-    
-    local old; old = hookfunction(debug.getproto, function(func, proto)
-      if func == Main.Controllers.PiggyBankController.KnitStart or func == Main.Controllers.CropController.KnitStart then
-        return FakeFunc
-      elseif func == FakeFunc then
-        return FakeFunc
-      end
-      return old(func, proto)
-    end)
+	    local Success, Main
+	    local FakeFunc = function() end
+	    repeat
+	        	Success, Main = pcall(function()
+	          			return debug.getupvalue(require(knit).setup, 9)
+	        	end)
+	        	task.wait()
+	    until Success
+	    
+	    local old; old = hookfunction(debug.getproto, function(func, proto)
+		        if func == Main.Controllers.PiggyBankController.KnitStart or func == Main.Controllers.CropController.KnitStart then
+		        		return FakeFunc
+		        elseif func == FakeFunc then
+		        		return FakeFunc
+		        end
+		        return old(func, proto)
+	    end)
 end
 
 local function downloadFile(path, func)
-	if not isfile(path) then
-		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
-		end)
-		if not suc or res == '404: Not Found' then
-			error(res)
+		if not isfile(path) then
+				local suc, res = pcall(function()
+						return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+				end)
+				if not suc or res == '404: Not Found' then
+						error(res)
+				end
+				if path:find('.lua') then
+						res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+				end
+				writefile(path, res)
 		end
-		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
-		end
-		writefile(path, res)
-	end
-	return (func or readfile)(path)
+		return (func or readfile)(path)
 end
 
 local function finishLoading()
-	vape.Init = nil
-	vape:Load()
-	task.spawn(function()
-		repeat
-			vape:Save()
-			task.wait(10)
-		until not vape.Loaded
-	end)
-
-	local teleportedServers
-	vape:Clean(lplr.OnTeleport:Connect(function()
-		if (not teleportedServers) and (not shared.VapeIndependent) then
-			teleportedServers = true
-			local teleportScript = [[
-				shared.vapereload = true
-				if shared.VapeDeveloper then
-					loadstring(readfile('newvape/loader.lua'), 'loader')()
-				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+		vape.Init = nil
+		vape:Load()
+		task.spawn(function()
+			repeat
+				vape:Save()
+				task.wait(10)
+			until not vape.Loaded
+		end)
+	
+		local teleportedServers
+		vape:Clean(lplr.OnTeleport:Connect(function()
+				if (not teleportedServers) and (not shared.VapeIndependent) then
+						teleportedServers = true
+						local teleportScript = [[
+								shared.vapereload = true
+								if shared.VapeDeveloper then
+										loadstring(readfile('newvape/loader.lua'), 'loader')()
+								else
+										loadstring(game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+								end
+						]]
+						if shared.VapeDeveloper then
+								teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
+						end
+						if shared.VapeCustomProfile then
+								teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
+						end
+						vape:Save()
+						queue_on_teleport(teleportScript)
 				end
-			]]
-			if shared.VapeDeveloper then
-				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
-			end
-			if shared.VapeCustomProfile then
-				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
-			end
-			vape:Save()
-			queue_on_teleport(teleportScript)
+		end))
+	
+		if not shared.vapereload then
+				if not vape.Categories then return end
+				if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
+						vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
+				end
 		end
-	end))
-
-	if not shared.vapereload then
-		if not vape.Categories then return end
-		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-			vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
-		end
-	end
 end
 
 if not isfile('newvape/profiles/gui.txt') then
-	writefile('newvape/profiles/gui.txt', 'rise')
+		writefile('newvape/profiles/gui.txt', 'rise')
 end
 local gui = readfile('newvape/profiles/gui.txt')
 
 if not isfolder('newvape/assets/'..gui) then
-	makefolder('newvape/assets/'..gui)
+		makefolder('newvape/assets/'..gui)
 end
 vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
 shared.vape = vape
 
 if not shared.VapeIndependent then
-	loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
-	if isfile('newvape/games/'..game.PlaceId..'.lua') then
-		loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-	else
-		if not shared.VapeDeveloper then
-			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
-			end)
-			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-			end
+		loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
+		if isfile('newvape/games/'..game.PlaceId..'.lua') then
+				loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+		else
+				if not shared.VapeDeveloper then
+						local suc, res = pcall(function()
+								return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+						end)
+						if suc and res ~= '404: Not Found' then
+								loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+				        end
+		        end
 		end
-	end
-	finishLoading()
+		finishLoading()
 else
-	vape.Init = finishLoading
-	return vape
+		vape.Init = finishLoading
+		return vape
 end
